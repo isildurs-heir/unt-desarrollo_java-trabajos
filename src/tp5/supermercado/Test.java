367f8ddb33cda403a2d28a266f099dde4c456540
Javier <114532940+isildurs-heir@users.noreply.github.com>
2023-03-15 22:59:15 -0300

    Update Test.java

--- src/tp5/supermercado/Test.java
+++ src/tp5/supermercado/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         Producto p1 = new Producto("jabon",35.23);
         Producto p2 = new Producto("jamon",25.14);
-        Cliente c1 = new Cliente("Javier",40792902);
+        Cliente c1 = new Cliente("Javier",154897632);
         c1.agregarProducto(p1, 2);
         c1.agregarProducto(p2,4);
         c1.mostrarCarrito();
